7a0bc66b5fe235c6ad600044e8ac2609d1a9139b
sumit9722 <shahsumit9722@gmail.com>
2025-02-02 20:23:11 +0530

    UI:Sidebar done except font and hoverState

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,47 +1,63 @@
 import styles from "../styles/Sidebar.module.scss";
+import { useState } from "react";
 
+import overview from "/svgs/overview.svg";
+import myProfile from "/svgs/myProfile.svg";
+import dues from "/svgs/dues.svg";
+import certificates from "/svgs/certificates.svg";
+import scholarships from "/svgs/scholarships.svg";
+import travelConcessions from "/svgs/travelConcessions.svg";
+import clubs from "/svgs/clubs.svg";
+import raf from "/svgs/raf.svg";
+import passportAndMess from "/svgs/passportAndMess.svg";
+import uploadingPhotoForIdCard from "/svgs/uploadingPhotoForIdCard.svg";
+import duplicateIdCard from "/svgs/duplicateIdCard.svg";
+import orientationProgram from "/svgs/orientationProgram.svg";
+import homeBtn from "/svgs/homeBtn.svg";
+
+const menuItems = [
+  { icon: overview, name: "Overview" },
+  { icon: myProfile, name: "My Profile" },
+  { icon: dues, name: "Dues" },
+  { icon: certificates, name: "Certificates" },
+  { icon: scholarships, name: "Scholarships" },
+  { icon: travelConcessions, name: "Travel Concessions" },
+  { icon: clubs, name: "Clubs" },
+  { icon: raf, name: "RAF" },
+  { icon: passportAndMess, name: "Passport & Mess" },
+  { icon: uploadingPhotoForIdCard, name: "Uploading Photo for ID Card" },
+  { icon: duplicateIdCard, name: "Duplicate ID Card" },
+  { icon: orientationProgram, name: "Orientation Program" },
+];
 export default function Sidebar() {
+  const [selectedMenuItem, setSelectedMenuItem] = useState(0);
+  const handleMenuItemClick = (index) => {
+    setSelectedMenuItem(index);
+  };
   return (
     <nav className={styles.sidebar}>
-      <a href="#" className={styles.menuItem}>
+      <div className={styles.homebtn}>
+        <img src={homeBtn} alt="Home" />
         SWD Home
-      </a>
-      <a href="#" className={styles.menuItem + " " + styles.active}>
-        Overview
-      </a>
-      <a href="#" className={styles.menuItem}>
-        My Profile
-      </a>
-      <a href="#" className={styles.menuItem}>
-        Dues
-      </a>
-      <a href="#" className={styles.menuItem}>
-        Certificates
-      </a>
-      <a href="#" className={styles.menuItem}>
-        Scholarships
-      </a>
-      <a href="#" className={styles.menuItem}>
-        Travel Concessions
-      </a>
-      <a href="#" className={styles.menuItem}>
-        Clubs
-      </a>
-      <a href="#" className={styles.menuItem}>
-        RAF
-      </a>
-      <a href="#" className={styles.menuItem}>
-        Passport and Mess
-      </a>
-      <a href="#" className={styles.menuItem}>
-        Uploading Photo for ID Card
-      </a>
-      <a href="#" className={styles.menuItem}>
-        Duplicate ID Card
-      </a>
-      <a href="#" className={styles.menuItem}>
-        Orientation Program
-      </a>
+      </div>
+      <div className={styles.menu}>
+        {menuItems.map((item, index) => (
+          <div
+            key={index}
+            className={
+              index === selectedMenuItem
+                ? styles.activeMenuItem
+                : styles.menuItem
+            }
+            onClick={() => {
+              handleMenuItemClick(index);
+            }}
+          >
+            <img src={item.icon} alt={item.name} className={styles.menuIcon} />
+            {item.name}
+          </div>
+        ))}
+      </div>
     </nav>
   );
 }
