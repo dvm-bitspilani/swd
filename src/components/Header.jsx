9780abdd1ce3aff8dee5b3b25932def0a9b8afdc
sumit9722 <shahsumit9722@gmail.com>
2025-02-03 16:51:21 +0530

    UI:Header and Sidebar Font

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,26 +7,19 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.logoSection}>
-        <img
-          src={bitsLogo}
-          alt="BITS Pilani Logo"
-          className={styles.logo}
-        />
+        <img src={bitsLogo} alt="BITS Pilani Logo" className={styles.logo} />
         <div className={styles.campusDetails}>
           <h1 className={styles.title}>BITS Pilani</h1>
           <p className={styles.campus}>Pilani Campus</p>
         </div>
       </div>
-      <h1 className={styles.title}>Student Welfare Division</h1>
+      <h1 className={styles.pageTitle}>Student Welfare Division</h1>
       <div className={styles.userSection}>
-        <img
-          src={icon}
-          alt="User"
-          className={styles.userImage}
-        />
+        <img src={icon} alt="User" className={styles.userImage} />
         <div className={styles.userDetails}>
           <div className={styles.username}>Chill Guy Ramesh</div>
-          <div className={styles.userId}>2024CHGYXXXXP
+          <div className={styles.userId}>
+            2024CHGYXXXXP
             <div className={styles.userMenuDropDown}>
               <img
                 src={dropdownArrow}
