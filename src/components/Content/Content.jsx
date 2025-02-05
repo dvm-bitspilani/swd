600b0807781e484950ef67f41eafc02f3147c209
Pranjal Gupta <pranjalgupta58.97@gmail.com>
2025-02-05 22:58:23 +0530

    Revert "Merge branch 'PJ' of https://github.com/dvm-bitspilani/swd"
    This reverts commit 8bb832731c1b08d68dcecfb9873da98acdaa5b54, reversing
    changes made to 00a677dfc19a6c99aa16d21b35b19c9dd51ce4e8.

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,4 +1,8 @@
 import Overview from "../contentPages/Overview/Overview";
+<<<<<<< HEAD
+=======
+import Certificates from "../contentPages/Certificates/Certificates";
+>>>>>>> parent of 8bb8327 (Merge branch 'PJ' of https://github.com/dvm-bitspilani/swd)
 import MyProfile from "../contentPages/MyProfile/MyProfile";
 import Navigator from "../Navigator/Navigator";
 import styles from "./Content.module.scss";
@@ -7,7 +11,11 @@ export default function Content() {
   return (
     <main className={styles.content}>
       <Navigator />
+<<<<<<< HEAD
       <MyProfile />
+=======
+      <Certificates></Certificates>
+>>>>>>> parent of 8bb8327 (Merge branch 'PJ' of https://github.com/dvm-bitspilani/swd)
     </main>
   );
 }
