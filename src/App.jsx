8139db666b5ee14c7716886dd5eadaf806930479
sumit9722 <shahsumit9722@gmail.com>
2025-02-09 19:46:59 +0530

    Fixes:minor bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import styles from "./app.module.scss";
 
 export default function App() {
   console.log(router);
