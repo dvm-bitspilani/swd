77f870db4fecc0e57cc294542b503ad0c0ef6e36
sumit9722 <shahsumit9722@gmail.com>
2025-02-10 19:22:42 +0530

    UI: ham menu version 1

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,15 +5,27 @@ import pages from "/src/assets/Pages";
 import { NavLink } from "react-router-dom";
 
 export default function Sidebar() {
-  const [activePage, setActivePage] = useState("Overview");
-  const handleMenuItemClick = (name) => {
-    setActivePage(name);
+  const [isClicked, setIsClicked] = useState(false);
+  const handleClick = () => {
+    setIsClicked((prev) => !prev);
   };
   return (
     <nav className={styles.sidebar}>
       <div className={styles.homebtn}>
         <img src={homeBtn} alt="Home" className={styles.homebtnIcon} />
         SWD Home
+      </div>
+      <div className={styles.hamburgerContainer}>
+        <div
+          className={isClicked ? styles.clicked : styles.hamburger}
+          onClick={handleClick}
+        >
+          <div className={styles.linesbars}>
+            <div className={`${styles.line} ${styles.line1}`}></div>
+            <div className={`${styles.line} ${styles.line2}`}></div>
+            <div className={`${styles.line} ${styles.line3}`}></div>
+          </div>
+        </div>
       </div>
       <div className={styles.menu}>
         {pages.map((item, index) => (
@@ -22,14 +34,14 @@ export default function Sidebar() {
             to={item.url}
             className={({ isActive, isPending }) =>
               isPending
-                ? "pending"
+                ? styles.pending
                 : isActive
                 ? styles.activeMenuItem
                 : styles.menuItem
             }
           >
             <img src={item.icon} alt={item.name} className={styles.menuIcon} />
-            {item.name}
+            <div className={styles.itemName}>{item.name}</div>
           </NavLink>
         ))}
       </div>
