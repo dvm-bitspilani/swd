28173e688e6b2ef22900a69be25aa0a0187c54bd
sumit9722 <shahsumit9722@gmail.com>
2026-01-16 01:50:54 +0530

    spelling mistake fixed for hosting

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
-import styles from "./app.module.scss";
+import styles from "./App.module.scss";
 
 export default function App() {
   return <RouterProvider router={router} />;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,30 +4,34 @@ import pages from "/src/assets/Pages";
 import { NavLink } from "react-router-dom";
 
 export default function Sidebar() {
-  return (
-    <nav className={styles.sidebar}>
-      <div className={styles.homebtn}>
-        <img src={homeBtn} alt="Home" className={styles.homebtnIcon} />
-        SWD Home
-      </div>
-      <div className={styles.menu}>
-        {pages.map((item, index) => (
-          <NavLink
-            key={index}
-            to={item.url}
-            className={({ isActive, isPending }) =>
-              isPending
-                ? "pending"
-                : isActive
-                ? styles.activeMenuItem
-                : styles.menuItem
-            }
-          >
-            <img src={item.icon} alt={item.name} className={styles.menuIcon} />
-            {item.name}
-          </NavLink>
-        ))}
-      </div>
-    </nav>
-  );
+    return (
+        <nav className={styles.sidebar}>
+            <div className={styles.homebtn}>
+                <img src={homeBtn} alt="Home" className={styles.homebtnIcon} />
+                SWD Home
+            </div>
+            <div className={styles.menu}>
+                {pages.map((item, index) => (
+                    <NavLink
+                        key={index}
+                        to={item.url}
+                        className={({ isActive, isPending }) =>
+                            isPending
+                                ? "pending"
+                                : isActive
+                                    ? styles.activeMenuItem
+                                    : styles.menuItem
+                        }
+                    >
+                        <div className={styles.icon}>
+                            <img src={item.icon} alt={item.name} className={styles.menuIcon} />
+                        </div>
+                        <div className={styles.text}>
+                            {item.name}
+                        </div>
+                    </NavLink>
+                ))}
+            </div>
+        </nav>
+    );
 }
